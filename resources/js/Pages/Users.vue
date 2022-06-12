c278bca68b05c48736e75e14007e831e7c403744
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-12 21:50:45 +0900

    preserve the scroll position when refresh time on User page

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -6,7 +6,7 @@
     <div style="margin-top: 400px">
         <p>The current time is {{ time }}.</p>
 
-        <Link href="/users" class="text-blue-500">Refresh</Link>
+        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
     </div>
 </template>
 
