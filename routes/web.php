f0ef0bb50f2794cad46fe312e1530e245290438d
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-12 20:34:32 +0900

    rename Welcome view to Home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return inertia('Welcome');
+    return inertia('Home', [
+        'name' => 'Jeffrey Way',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
+    ]);
 });
