b6e3b815d76ea00ba24b90597bfabb4299f2fb9f
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-12 21:08:28 +0900

    apply Nav component to all the Pages

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,13 @@
 <template>
     <h1>Home</h1>
+
+    <Nav />
 </template>
 
 <script>
-export default {
+import Nav from '../Shared/Nav';
 
+export default {
+    components: { Nav },
 };
 </script>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,9 +1,13 @@
 <template>
     <h1>Users</h1>
+
+    <Nav />
 </template>
 
 <script>
-export default {
+import Nav from '../Shared/Nav';
 
+export default {
+    components: { Nav },
 };
 </script>
