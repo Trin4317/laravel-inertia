acb2dae656bd74bfe20bacfe34e1271724aa472e
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-12 22:13:57 +0900

    extract Link from Nav component to its own component

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -2,28 +2,22 @@
     <nav class="mt-6">
         <ul class="list-disc">
             <li>
-                <Link href="/" class="text-blue-500 hover:underline"
-                    :class="{ 'font-bold underline': $page.component === 'Home' }">Home</Link>
+                <NavLink href="/" :active="$page.component === 'Home'">Home</NavLink>
             </li>
             <li>
-                <Link href="/users" class="text-blue-500 hover:underline"
-                    :class="{ 'font-bold underline': $page.component === 'Users' }">Users</Link>
+                <NavLink href="/users" :active="$page.component === 'Users'">Users</NavLink>
             </li>
             <li>
-                <Link href="/settings" class="text-blue-500 hover:underline"
-                    :class="{ 'font-bold underline': $page.component === 'Settings' }">Settings</Link>
-            </li>
-            <li>
-                <Link href="/logout" method="post" as="button" class="text-blue-500 hover:underline">Log out</Link>
+                <NavLink href="/settings" :active="$page.component === 'Settings'">Settings</NavLink>
             </li>
         </ul>
     </nav>
 </template>
 
 <script>
-import { Link } from '@inertiajs/inertia-vue3';
+import NavLink from '../Shared/NavLink';
 
 export default {
-    components: { Link },
+    components: { NavLink },
 };
 </script>
