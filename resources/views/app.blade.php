68e9a7c61eaa67131c593cdee65923b7f18ae80a
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-13 23:00:31 +0900

    use Mix to extract standard vendor javascript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src="{{ mix('/js/manifest.js') }}" defer></script>
+    <script src="{{ mix('/js/vendor.js') }}" defer></script>
     @inertiaHead
   </head>
   <body>
