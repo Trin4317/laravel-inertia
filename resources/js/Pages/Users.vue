f485ff5dbc7c49a642a33ecefdf8ca8943e704f1
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-13 21:23:43 +0900

    apply Layout to Pages components

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,21 +1,22 @@
 <template>
-    <h1>Users</h1>
+    <Layout>
+        <h1 class="text-3xl">Users</h1>
 
-    <Nav />
+        <div style="margin-top: 400px">
+            <p>The current time is {{ time }}.</p>
 
-    <div style="margin-top: 400px">
-        <p>The current time is {{ time }}.</p>
+            <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+        </div>
+    </Layout>
 
-        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-    </div>
 </template>
 
 <script>
-import Nav from '../Shared/Nav';
+import Layout from '../Shared/Layout';
 import { Link } from '@inertiajs/inertia-vue3';
 
 export default {
-    components: { Nav, Link },
+    components: { Layout, Link },
     props: {
         time: String,
     }
