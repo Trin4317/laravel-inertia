14546c941378ef063fd21ba12e4b15c99578ddd1
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-14 21:46:14 +0900

    add paginator to the bottom of Users page

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -34,6 +34,13 @@
       </div>
     </div>
     </div>
+
+    <div class="mt-6">
+        <template v-for="link in users.links">
+            <Link v-if="link.url" :href="link.url" v-html="link.label" />
+            <span v-else v-html="link.label"></span>
+        </template>
+    </div>
 </template>
 
 <script setup>
