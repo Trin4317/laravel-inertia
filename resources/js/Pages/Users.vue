637750cd51f49e9dc6613310c71ed714516fed57
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-14 22:21:02 +0900

    use dynamic component instead of v-if-else

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -6,40 +6,47 @@
     <h1 class="text-3xl mb-6">Users</h1>
 
     <div class="flex flex-col">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-          <table class="min-w-full divide-y divide-gray-200">
-            <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="user in users.data" :key="user.id">
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="flex items-center">
-                    <div>
-                      <div class="text-sm font-medium text-gray-900">
-                        {{ user.name }}
-                      </div>
-                    </div>
-                  </div>
-                </td>
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr v-for="user in users.data" :key="user.id">
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div>
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ user.name }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
 
-                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <Link :href="`/users/${user.id}/edit`" class="text-indigo-600 hover:text-indigo-900">
-                    Edit
-                  </Link>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <Link :href="`/users/${user.id}/edit`"
+                                        class="text-indigo-600 hover:text-indigo-900">
+                                    Edit
+                                    </Link>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     </div>
 
     <div class="mt-6">
-        <template v-for="link in users.links">
+        <!-- <template v-for="link in users.links">
             <Link v-if="link.url" :href="link.url" v-html="link.label" />
             <span v-else v-html="link.label"></span>
-        </template>
+        </template> -->
+        <Component
+            :is="link.url ? Link : 'span'"
+            v-for="link in users.links"
+            :href="link.url"
+            v-html="link.label"
+        />
     </div>
 </template>
 
