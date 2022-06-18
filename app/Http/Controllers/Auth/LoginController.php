32b39988c7fd39f30ee7fe2c209b2b21f24af3e2
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-19 08:05:21 +0900

    create Login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+
+class LoginController extends Controller
+{
+    public function create()
+    {
+        return Inertia::render('Auth/Login');
+    }
+}
