5d152d1ff912bafc770b669e3645bf6dda671c2b
Trin1437 <eg.artourbabaev@gmail.com>
2022-06-19 07:49:38 +0900

    group all the routes inside auth middleware

--- routes/web.php
+++ routes/web.php
@@ -14,52 +14,54 @@ use App\Models\User;
 |
 */
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return inertia('Home');
+    });
 
-Route::get('/users', function () {
-    return inertia('Users/Index', [
-        // using `map` here would return a new Array instead of Collection
-        // which means we wont have access to Links property that is neccessary for Pagination
-        // instead, we can use `through` to keep the same Collection and manipulate only the users.data Array
-        'users' => User::query()
-            // when there is a "search" key in the request
-            // append to the query what's inside the closure
-            ->when(request()->input('search'), function ($query, $search) {
-                $query->where('name', 'like', "%{$search}%");
-            })
-            ->paginate(10)
-            // include the query string in paginate URL
-            ->withQueryString()
-            ->through(fn($user) => [
-                'id' => $user->id,
-                'name' => $user->name
-            ]),
-        'filters' => request()->only(['search'])
-    ]);
-});
+    Route::get('/users', function () {
+        return inertia('Users/Index', [
+            // using `map` here would return a new Array instead of Collection
+            // which means we wont have access to Links property that is neccessary for Pagination
+            // instead, we can use `through` to keep the same Collection and manipulate only the users.data Array
+            'users' => User::query()
+                // when there is a "search" key in the request
+                // append to the query what's inside the closure
+                ->when(request()->input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate(10)
+                // include the query string in paginate URL
+                ->withQueryString()
+                ->through(fn ($user) => [
+                    'id' => $user->id,
+                    'name' => $user->name
+                ]),
+            'filters' => request()->only(['search'])
+        ]);
+    });
 
-Route::post('/users', function () {
-    $attributes = request()->validate([
-        'name' => 'required',
-        'email' => ['required','email'],
-        'password' => 'required'
-    ]);
+    Route::post('/users', function () {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
 
-    User::create($attributes);
+        User::create($attributes);
 
-    return redirect('/users');
-});
+        return redirect('/users');
+    });
 
-Route::get('/users/create', function () {
-    return inertia('Users/Create');
-});
+    Route::get('/users/create', function () {
+        return inertia('Users/Create');
+    });
 
-Route::get('/settings', function () {
-    return inertia('Settings');
-});
+    Route::get('/settings', function () {
+        return inertia('Settings');
+    });
 
-Route::post('/logout', function () {
-    dd('logging the user out');
+    Route::post('/logout', function () {
+        dd('logging the user out');
+    });
 });
